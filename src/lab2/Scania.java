d48e8e69d31270531065bb930c6f43d22bcada2d
herr-ek <43437993+herr-ek@users.noreply.github.com>
2023-11-23 09:14:41 +0100

    Scania lower/lift bed and bug fix á la Frogg

--- src/lab2/Scania.java
+++ src/lab2/Scania.java
@@ -7,7 +7,7 @@ public class Scania extends TruckBedTruck {
     public Scania() {
         super(18000 , 70);
         nrDoors = 2;
-        enginePower = 420;
+        enginePower = 100;
         color = Color.darkGray;
         modelName = "Scania";
         weight = 14000;
